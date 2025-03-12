d1e5ad7bd054ed1b74f5d8e8bfd92e088ee5a502
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-12 06:56:38 +0200

    Adding User model

---
+++ Gallery-Server/models/User_Skeleton.php
@@ -0,0 +1,15 @@
+<?php
+    class User_Skeleton{
+        public static $id;
+        public static $user_name;
+        public static $email;
+        public static $pass;
+
+        function create($email, $pass, $name = null, $id = -1){
+            self::$id=$id;
+            self::$user_name = $name;
+            self::$email = $email;
+            self::$pass = $pass;
+        }
+    }
+?>
