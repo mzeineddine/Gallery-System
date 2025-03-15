f1e00eab2deada8cde03809dc36dcd082539f3eb
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-15 22:46:35 +0200

    Adding id auto_inc and primary key

--- Gallery-Server/database/images_metadata_migration.php
+++ Gallery-Server/database/images_metadata_migration.php
@@ -2,7 +2,7 @@
 $base = "..";
     require $base . "/connection/connection.php";
     $query = $conn->prepare("CREATE TABLE IF NOT EXISTS`images_metadata` (
-                                        `id` int(11) NOT NULL,
+                                        `id` int(11) INT AUTO_INCREMENT PRIMARY KEY,
                                         `user_id` int(11) NOT NULL,
                                         `img` varchar(255) NOT NULL,
                                         `title` varchar(255) NOT NULL,
--- Gallery-Server/database/users_migration.php
+++ Gallery-Server/database/users_migration.php
@@ -2,7 +2,7 @@
     $base = "..";
     require $base . "/connection/connection.php";
     $query = $conn->prepare("CREATE TABLE IF NOT EXISTS `users` (
-                                                `id` int(11) NOT NULL,
+                                                `id` INT AUTO_INCREMENT PRIMARY KEY,
                                                 `user_name` varchar(255) NOT NULL,
                                                 `email` varchar(255) NOT NULL,
                                                 `pass` varchar(255) NOT NULL
