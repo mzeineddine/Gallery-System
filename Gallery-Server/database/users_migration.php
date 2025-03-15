0918d4c5afbe6e8f5a2cfbd30b7fbe8599e096af
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-15 22:24:03 +0200

    Fixing path users migrations

--- Gallery-Server/database/users_migration.php
+++ Gallery-Server/database/users_migration.php
@@ -1,7 +1,7 @@
 <?php
     $base = "..";
     require $base . "/connection/connection.php";
-    $query = $conn->prepare("CREATE TABLE IF NOT EXISTS CREATE TABLE `users` (
+    $query = $conn->prepare("CREATE TABLE IF NOT EXISTS `users` (
                                                 `id` int(11) NOT NULL,
                                                 `user_name` varchar(255) NOT NULL,
                                                 `email` varchar(255) NOT NULL,
